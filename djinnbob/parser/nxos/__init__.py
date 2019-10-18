e6bd6a6777c9514ca31fb38c22e856055cfb294f
Jeremy Schulman <jeremy.schulman@mlb.com>
2019-10-18 12:44:33 -0400

    init files

---
+++ djinnbob/parser/nxos/__init__.py
@@ -0,0 +1,4 @@
+from genie import abstract
+abstract.declare_package(__name__)
+
+from . import show_interface_transceiver
---
+++ setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+package_version = '0.1.0'
+package_name = 'djinnbob'
+
+
+def requirements(filename='requirements.txt'):
+    return open(filename.strip()).readlines()
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name=package_name,
+    version=package_version,
+    description='pyATS Genie - dynamic add parser',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Jeremy Schulman',
+    packages=find_packages(),
+    install_requires=requirements(),
+)
